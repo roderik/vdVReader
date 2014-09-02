04913f71ad1746ef4bda12774c5725a56a3a9ee5
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-09-02 07:47:11 +0200

    Remove iOS 8 code

--- vdvReader/AppDelegate.m
+++ vdvReader/AppDelegate.m
@@ -20,7 +20,7 @@
     // Override point for customization after application launch.
     UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
+    //navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
     splitViewController.delegate = self;
     return YES;
 }
